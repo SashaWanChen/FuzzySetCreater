ee561781a6a8a677bf4a3a8339c562380d6c4671
Wan-Chen Chen <enlen111408@gmail.com>
2021-01-23 23:01:38 +0800

    Update AssemblyInfo.cs
    Change Assembly Title and Assembly Product

--- FuzzySetCreater/Properties/AssemblyInfo.cs
+++ FuzzySetCreater/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("R09546008陳琬臻Ass06")]
+[assembly: AssemblyTitle("CreateFuzzySet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("R09546008陳琬臻Ass06")]
+[assembly: AssemblyProduct("CreateFuzzySet")]
 [assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
